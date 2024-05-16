d4d553b00740ebb95029780db1ab2ec89f836eb5
katrise <100132510+katrise@users.noreply.github.com>
2024-05-16 22:24:36 +0300

    (REAL) Create context object in new file

---
+++ react-state-test-context-v1/src/ContextBase.js
@@ -0,0 +1,5 @@
+//Lai lietotu Context API, tas jāimportē no React bibliotēkas
+import { createContext } from 'react';
+//Izveidojam konteksta objektu un eksportējam to, lai tas būtu pieejams ārpus šī faila
+export const ContextBase = createContext("");
+
