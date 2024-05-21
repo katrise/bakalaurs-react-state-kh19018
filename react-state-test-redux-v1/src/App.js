3d750e4e449af1c171497bbc7205edb7f9d8bc6e
katrise <100132510+katrise@users.noreply.github.com>
2024-05-22 01:11:25 +0300

    Update App component content

--- react-state-test-redux-v1/src/App.js
+++ react-state-test-redux-v1/src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import store from './app/store';
+import { Provider } from 'react-redux';
+import InsideComponent from './InsideComponent';
+import OutsideComponent from './OutsideComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+      <Provider store={store}>
+       <InsideComponent /> 
+      </Provider>
+      {/* <OutsideComponent /> */}
     </div>
   );
 }
