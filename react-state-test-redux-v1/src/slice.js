5adec8b0b8ffc46f35452d12b81f7ed315354598
katrise <100132510+katrise@users.noreply.github.com>
2024-05-22 02:26:05 +0300

    Add comments to slice file

--- react-state-test-redux-v1/src/slice.js
+++ react-state-test-redux-v1/src/slice.js
@@ -1,17 +1,22 @@
+//rīka importācija segmenta vieglākai pārvaldībai
 import { createSlice } from '@reduxjs/toolkit'
-
+//segments tiek izveidots ar 'createSlice' funkcijas palīdzību
 export const textSlice = createSlice({
+  //tiek piešķirts segmenta nosaukums
   name: 'texts',
   initialState: {
+    //noklusējuma vērtība
     value: '',
   },
   reducers: {
+    //reducenta funkcionalitātes izveide, kura pieņem stāvokli kā vērtību
     showText: (state) => {
+      //tiek iestatīta jauna stāvokļa vērtība
       state.value = 'Iekšējā komponente sūta sveicienus!'
     },
   },
 })
-
+//reducenta funkcionalitātes eksportācija
 export const { showText } = textSlice.actions;
 
 export default textSlice.reducer;
