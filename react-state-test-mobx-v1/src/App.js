6364ade7bb7ffb96ccc9919ce36044ebe99b1ed5
katrise <100132510+katrise@users.noreply.github.com>
2024-05-22 23:36:18 +0300

    Create App component modifications

--- react-state-test-mobx-v1/src/App.js
+++ react-state-test-mobx-v1/src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+//Izveidoto komponenšu importēšana
+import InsideComponent from './InsideComponent'
+import OutsideComponent from './OutsideComponent'
+//Pamata komponentes 'App' izveide
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* Iekšējās un ārējās komponentes izsaukšana */}
+        <InsideComponent />
+        <OutsideComponent />
       </header>
     </div>
   );
