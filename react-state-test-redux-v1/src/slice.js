2a752a59ee6ef81643e248b9f1ddc30f44984155
katrise <100132510+katrise@users.noreply.github.com>
2024-05-26 23:44:11 +0300

    Create Redux slice for complex

--- react-state-test-redux-v1/src/slice.js
+++ react-state-test-redux-v1/src/slice.js
@@ -1,22 +1,26 @@
-//rīka importācija segmenta vieglākai pārvaldībai
-import { createSlice } from '@reduxjs/toolkit'
-//segments tiek izveidots ar 'createSlice' funkcijas palīdzību
-export const textSlice = createSlice({
-  //tiek piešķirts segmenta nosaukums
-  name: 'texts',
+//rīkkopas metodes imports priekš segmenta izveides
+import { createSlice } from '@reduxjs/toolkit';
+
+export const listSlice = createSlice({
+  //noklusējuma vērtību iestatīšana
+  name: 'list',
   initialState: {
-    //noklusējuma vērtība
-    value: '',
+    //izveidojam 2500 garu masīvu ar aizpildītām vērtībām
+    items: Array.from({ length: 2500 }, (_, index) => `Item ${index + 1}`),
   },
+  //reducentu funkciju definēšana
   reducers: {
-    //reducenta funkcionalitātes izveide, kura pieņem stāvokli kā vērtību
-    showText: (state) => {
-      //tiek iestatīta jauna stāvokļa vērtība
-      state.value = 'Iekšējā komponente sūta sveicienus!'
+    //viena ieraksta pievienošana izmantojam push, lai pievienotu masīva galā jauno vērtību
+    addItem: (state, action) => {
+      state.items.push(action.payload);
+    },
+    //tāpat kā iepriekš pievienojam vērtības galā individuāli ar (...) palīdzību
+    addMultipleItems: (state, action) => {
+      state.items.push(...action.payload);
     },
   },
-})
-//reducenta funkcionalitātes eksportācija
-export const { showText } = textSlice.actions;
-
-export default textSlice.reducer;
+});
+//eksportējam reducenta funkcijas kā darbības, lai tās varētu pielietot ārpus segmenta
+export const { addItem, addMultipleItems } = listSlice.actions;
+//eksportējam reducenta metodes, lai varētu veikt darbības ar stāvokļiem
+export default listSlice.reducer;
