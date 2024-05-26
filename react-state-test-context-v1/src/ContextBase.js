64f2b3dbf29971caed884fb8e5d364986248db02
katrise <100132510+katrise@users.noreply.github.com>
2024-05-26 17:42:14 +0300

    Create context file for Context API big data test

--- react-state-test-context-v1/src/ContextBase.js
+++ react-state-test-context-v1/src/ContextBase.js
@@ -1,5 +1,39 @@
-//Lai lietotu Context API, tas jāimportē no React bibliotēkas
-import { createContext } from 'react';
-//Izveidojam konteksta objektu un eksportējam to, lai tas būtu pieejams ārpus šī faila
-export const ContextBase = createContext("");
-
+//Nepieciešamo bilbiotēkas rīku imports
+import { createContext, useReducer, useContext } from 'react';
+//Tiek izveidots konteksta objekts, kurš pārvietos un uzturēs stāvokli
+const StateContext = createContext();
+//Iestata noklusējuma stāvokļa vērtības
+const initialState = {
+  //Masīva veida objekts garumā 2500, kur katrai vērtībai tiek piešķirts
+  //ieraksts "Item (index)"
+  items: Array.from({ length: 2500 }, (_, index) => `Item ${index + 1}`),
+};
+//Reducenta funkciju loģikas izveide
+function reducer(state, action) {
+  switch (action.type) {
+    case 'ADD_ITEM': //viena ieraksta pievienošana
+    //izmantojam "...", lai nepārrakstītu stāvoklī esosās vertības
+    //iekš items izveidojam jau esošo stāvokļa vērtību sarakstu, papildinam to
+    //ar action.payload, kas satur jaunās vērtības, kas iegūtas no darbības izpildes
+      return { ...state, items: [...state.items, action.payload] };
+    case 'ADD_ITEMS': //2500 ierakstu pievienošana
+      return { ...state, items: [...state.items, ...action.payload] };
+    default:
+      return state;
+  }
+}
+//StateProvider funkcija pārvaldīs stāvokli komponentēm, kuras tiks pievienotas
+//kā bērna komponentes <StateProvider /> (iekš App.js)
+export const StateProvider = ({ children }) => {
+  //izveidojam reducētāju, kurš apstrādās stāvokļa loģiku
+  const [state, dispatch] = useReducer(reducer, initialState);
+  //funkcionalitāte augstāk minētajam <StateProvider />, kurā nodrošina piekļuvi
+  //stāvoklim un tā apstrādes funkcijām
+  return (
+    <StateContext.Provider value={{ state, dispatch }}>
+      {children}
+    </StateContext.Provider>
+  );
+};
+//izveidoto kontekstu eksportē, lai to varētu izmantot ārpus šī faila
+export const useStateContext = () => useContext(StateContext);
