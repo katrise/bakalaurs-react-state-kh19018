6416dc5de6142c3c1f00464bc0bc6f0e1d41b021
katrise <100132510+katrise@users.noreply.github.com>
2024-05-26 18:21:01 +0300

    Item display component creation

---
+++ react-state-test-context-v1/src/components/ItemList.js
@@ -0,0 +1,22 @@
+//konteksta metodes imports
+import { useStateContext } from '../ContextBase';
+//komponente ItemList pārraida vizuālu sarakstu ar tā vērtībām
+function ItemList () {
+  //izveidojam stāvokļa mainīgā vērtību, kurš pārraidīs stāvokļa vērtības
+  const { state } = useStateContext();
+
+  return (
+    <div>
+      <h2>Items</h2>
+      <ul>
+        {/* Iterē cauri stāvokļa satura sarakstam ar funkciju map katram ierakstam
+        pievienojo identificējošu atslēgu un pārraiddot tā vērtību */}
+        {state.items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ItemList;
